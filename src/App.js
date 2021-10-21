d2479fb87a2614e02364d23119c509b5899fd12d
tylerdance <tylerdance12@gmail.com>
2021-10-21 17:30:00 -0400

    misc in

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import rootrot from './components/rootrot';
 import SelfPres from './components/SelfPres';
 import SoundsLike from './components/SoundsLike';
 import RubberCityHall from './components/RubberCityHall';
+import Misc from './components/Misc';
 // import Footer from './components/Footer';
 // import FallingCredits from './components/FallingCredits';
 // import Embed from 'react-music-embed'
@@ -25,6 +26,7 @@ function App() {
         <Route exact path='/selfpres' component={ SelfPres } />
         <Route exact path='/soundslike' component={ SoundsLike } />
         <Route exact path='/rubbercityhall' component={ RubberCityHall } />
+        <Route exact path='/misc' component={ Misc } />
         {/* <Route exact path='/fallingcredits' component={ FallingCredits } /> */}
 
 
---
+++ src/components/Misc.js
@@ -0,0 +1,24 @@
+function Misc () {
+    return (
+        <div>
+            <div>
+                <div className="misc-div">
+                    <img className="pic-square" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723581/lms%20misc/misc1_hrs9zk.jpg" alt="" />
+                    <img className="pic-square" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723581/lms%20misc/misc2_xoj1bl.jpg" alt="" />
+                    <img className="pic-square" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723580/lms%20misc/misc3_ctkmjg.jpg" alt="" />
+                    <img className="pic-square" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723581/lms%20misc/misc4_pgph6a.jpg" alt="" />
+                    <img className="pic-square" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723580/lms%20misc/misc5_q3fih9.jpg" alt="" />
+                    <img className="pic-square" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723582/lms%20misc/misc6_a5doiu.jpg" alt="" />
+                    <img className="pic-square" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723581/lms%20misc/misc7_ekcuxq.jpg" alt="" />
+                    <img className="pic-square" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723581/lms%20misc/misc8_hrclnj.jpg" alt="" />
+                    <img className="pic-square" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723581/lms%20misc/misc9_bskyve.jpg" alt="" />
+                    <img className="pic-square" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723582/lms%20misc/misc10_b419d0.jpg" alt="" />
+                    <img className="pic-square" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723582/lms%20misc/misc11_tbkwm7.jpg" alt="" />
+                    <img className="pic-square" src="" alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Misc;
