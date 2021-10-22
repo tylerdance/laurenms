2e7b9b63772aaabfed923bce8c3eb04f2ad7bab3
tylerdance <tylerdance12@gmail.com>
2021-10-22 10:34:54 -0400

    fam in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
-// import MainVideo from './components/MainVideo';
-// import Falling from './components/Falling';
 // import Nav from './components/Nav';
 // import Contact from './components/Contact';
 import Home from './components/Home';
@@ -12,6 +10,7 @@ import RubberCityHall from './components/RubberCityHall';
 import Misc from './components/Misc';
 import RubberAndFriends from './components/RubberAndFriends';
 import RubberWCL from './components/RubberWCL';
+import Fam from './components/Fam';
 // import Footer from './components/Footer';
 // import FallingCredits from './components/FallingCredits';
 // import Embed from 'react-music-embed'
@@ -19,7 +18,7 @@ import RubberWCL from './components/RubberWCL';
 function App() {
   return (
     <div className="App">
-      {/* <h5 id="header">Sugiwon</h5> */}
+      {/* <h5 id="header"></h5> */}
       <BrowserRouter>
         {/* <Nav /> */}
         <Route exact path='/' component={ Home } />
@@ -31,8 +30,7 @@ function App() {
         <Route exact path='/misc' component={ Misc } />
         <Route exact path='/RubberAndFriends' component={ RubberAndFriends } />
         <Route exact path='/RubberWCL' component={ RubberWCL } />
-        {/* <Route exact path='/fallingcredits' component={ FallingCredits } /> */}
-
+        <Route exact path='/Fam' component={ Fam } />
 
       </BrowserRouter>
 
---
+++ src/components/Fam.js
@@ -0,0 +1,15 @@
+function Fam () {
+    return (
+        <div>
+            <div>
+                <div className="fam-div">
+                    <img className="pic-square" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723474/lms%20fam/fam1_aohlw5.jpg" alt="" />
+                    <img className="pic-square" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723475/lms%20fam/fam2_myjpfe.jpg" alt="" />
+                    <img className="pic-square" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723474/lms%20fam/fam3_shmux3.jpg" alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Fam;
