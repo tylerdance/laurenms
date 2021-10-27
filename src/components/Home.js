5384c8f1076c57f8138ea8fef13dbd26aa36f87a
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2021-10-27 08:39:29 -0400

    hover effect on 2

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,10 @@ function Home () {
                         <Link id="selfpres" to="/selfpres"><p className="home-text">Major Van Winkle - Selfish Presley EP</p></Link>
                     </div>
                     
-                    <div><Link id="soundslike" to="/soundslike"><img className="home-pics" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723806/lms%20sounds%20like/soundslike18_vvdghe.jpg" alt="" /></Link></div>
+                    <div className="home-pics-subdiv">
+                        <Link id="soundslike" to="/soundslike"><img className="home-pics" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723806/lms%20sounds%20like/soundslike18_vvdghe.jpg" alt="" /></Link>
+                        <Link id="soundslike" to="/soundslike"><p className="home-text">Great Time - "Sounds Like ____" EP Release</p></Link>
+                    </div>
 
                     <div id="spliffbreak-vid-div">
                         <div className='player-wrapper'>
@@ -28,10 +31,26 @@ function Home () {
                         </div>
                     </div>
 
-                    <div><Link id="rubber-cityhall" to="/rubbercityhall"><img className="home-pics" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723613/lms%20rubber%20city%20hall/rubbercityhall2_g6yh4k.jpg" alt="" /></Link></div>
-                    <div><Link id="misc" to="/misc"><img className="home-pics" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723581/lms%20misc/misc8_hrclnj.jpg" alt="" /></Link></div>
-                    <div><Link id="rubberandfriends" to="/rubberandfriends"><img className="home-pics" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723654/lms%20rubber%20w%20friends/rubberwfriends13_czh2oh.jpg" alt="" /></Link></div>
-                    <div><Link id="rubberwcl" to="/rubberwcl"><img className="home-pics" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723712/lms%20rubber%20wcl/rubberwcl1_j1k0de.jpg" alt="" /></Link></div>
+                    <div className="home-pics-subdiv">
+                        <Link id="rubber-cityhall" to="/rubbercityhall"><img className="home-pics" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723613/lms%20rubber%20city%20hall/rubbercityhall2_g6yh4k.jpg" alt="" /></Link>
+                        <Link id="rubber-cityhall" to="/rubbercityhall"><p className="home-text">Rubber Live at City Hall</p></Link>
+                    </div>
+                    
+                    <div className="home-pics-subdiv">
+                        <Link id="misc" to="/misc"><img className="home-pics" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723581/lms%20misc/misc8_hrclnj.jpg" alt="" /></Link>
+                        <Link id="misc" to="/misc"><p className="home-text">misc</p></Link>
+                    </div>
+                        
+                    <div className="home-pics-subdiv">
+                        <Link id="rubberandfriends" to="/rubberandfriends"><img className="home-pics" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723654/lms%20rubber%20w%20friends/rubberwfriends13_czh2oh.jpg" alt="" /></Link>
+                        <Link id="rubberandfriends" to="/rubberandfriends"><p className="home-text">Rubber with Suzzanne Sheer, Taylor Kelly, and CJ Mills</p></Link>
+                    </div>
+                        
+                    <div className="home-pics-subdiv">
+                        <Link id="rubberwcl" to="/rubberwcl"><img className="home-pics" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723712/lms%20rubber%20wcl/rubberwcl1_j1k0de.jpg" alt="" /></Link>
+                        <Link id="rubberwcl" to="/rubberwcl"><p className="home-text">misc</p></Link>
+                    </div>
+                        
                     <div><Link id="fam" to="/fam"><img className="home-pics" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723474/lms%20fam/fam3_shmux3.jpg" alt="" /></Link></div>
                     <div><Link id="livestream" to="/livestream"><img className="home-pics" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723545/lms%20livestream/livestream4_gnllli.jpg" alt="" /></Link></div>
                     <div><Link id="rubberwawa" to="/rubberwawa"><img className="home-pics" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1633723683/rubber%20wawa/rubberwawa15_k9cnrh.jpg" alt="" /></Link></div>
